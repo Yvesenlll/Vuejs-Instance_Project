e8d2f4e953f2567aa7330f7b49cbf7398a0183f2
Yvesen <yvesenlll@outlook.com>
2021-05-26 11:27:16 -0400

    Attribute binding/ Conditional Rendering/ List Rendering/ Even Handling

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-var app = new VTTCue({
-    el: 'app',
+var app = new Vue({
+    el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        discription: 'this a description',
+        image: 'vmSocks-green-onWhite.jpeg',
+        inStock: true,
+        details: ["80% contton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green',
+                variantImage: 'vmSocks-green-onWhite.jpeg'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue',
+                variantImage: 'vmSocks-blue-onWhite.jpeg'
+            }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart: function (){
+            this.cart += 1;
+        },
+        updateProduct(variantImage){
+            this.image = variantImage;
+        }
     }
 })
